f32176150be72585aa09c1fb3aedf352d332b3cf
Alex Rehnby <alexr@bitquilltech.com>
2022-04-14 13:26:58 -0400

    Fix parsing of new option

--- ODBC/OAuthSampleApp.cpp
+++ ODBC/OAuthSampleApp.cpp
@@ -173,7 +173,7 @@ int main(int argc, char **argv)
     while (1)
     {
         int option_index = 0;
-        c = getopt_long(argc, argv, "a:b:c:d:e:f", long_options, &option_index);
+        c = getopt_long(argc, argv, "a:b:c:d:e:f:", long_options, &option_index);
         if (c == -1)
             break;
         switch (c)
