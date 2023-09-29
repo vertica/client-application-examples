53c0d5aa04e13ca7d71f8c326778020ef48ab5e5
Bill Jones <william.jones@microfocus.com>
2023-09-29 13:43:47 -0400

    Addressed PR review comments.

--- ADO.NET/ResultSetPrinter.cs
+++ ADO.NET/ResultSetPrinter.cs
@@ -6,29 +6,29 @@ using System.Collections.ObjectModel;
 using System.Linq;
 
 internal class ResultSetPrinter {
-        private int m_maxColumnWidth = Int32.Parse (ConfigurationManager.AppSettings["MaxColumnWidth"]);
-        private int m_widestColumnRead = 0;
+        private const string TRUNCATION_SYMBOL = "...";
+
+        private int  m_maxColumnWidth   = Int32.Parse(ConfigurationManager.AppSettings["MaxColumnWidth"]);
+        private int  m_widestColumnRead = 0;
+        private bool m_ValuesTruncated  = false;
         private VerticaDataReader m_reader;
         private ReadOnlyCollection<DbColumn> m_columns;
-        private const string TRUNCATION_SYMBOL = "...";
-        private bool m_ValuesTruncated = false;
 
-        public ResultSetPrinter (VerticaDataReader reader) {
+
+        public ResultSetPrinter(VerticaDataReader reader) {
             m_reader = reader;
             m_columns = m_reader.GetColumnSchema();
         }
 
-        public void printResults () 
+        public void printResults() 
         {
-            printColumnHeaders ();
+            printColumnHeaders();
 
             // Write the data
-            while (m_reader.Read())
-            {
-                for (int i = 0; i < m_reader.FieldCount; i++)
-                {
-                    string Value = m_reader.GetValue(i).ToString ();
-                    Console.Write("{0} ", formatColumnValue (m_columns[i], Value));
+            while (m_reader.Read()) {
+                for (int i = 0; i < m_reader.FieldCount; i++) {
+                    string Value = m_reader.GetValue(i).ToString();
+                    Console.Write("{0} ", formatColumnValue(m_columns[i], Value));
                 }
                 Console.WriteLine();
             }
@@ -40,65 +40,65 @@ internal class ResultSetPrinter {
             }
         }
         
-        private void printColumnHeaders () 
+        private void printColumnHeaders() 
         {
-            printColumnSeparator ();
+            printColumnSeparator();
 
             // Write the column headers
-            foreach (DbColumn column in m_columns)
-            {
-                int columnWidth = getPrintedColumnWidth (column);
-                checkColumnWidth (columnWidth);
-                Console.Write("{0} ", column.ColumnName.PadRight (columnWidth));
+            foreach (DbColumn column in m_columns) {
+                int columnWidth = getPrintedColumnWidth(column);
+                checkColumnWidth(columnWidth);
+                Console.Write("{0} ", column.ColumnName.PadRight(columnWidth));
             }
             Console.WriteLine();
 
-            printColumnSeparator ();
+            printColumnSeparator();
         }
 
-        private string formatColumnValue (DbColumn column, string Value)
+        private string formatColumnValue(DbColumn column, string Value)
         {
-            checkColumnWidth (Value.Length);
-            int columnWidth = getPrintedColumnWidth (column);
-            if (Value.Length > columnWidth) {
+            checkColumnWidth(Value.Length);
+            int columnWidth = getPrintedColumnWidth(column);
+            if(Value.Length > columnWidth) {
                 m_ValuesTruncated = true;
                 // Truncate value if its too long
-                return Value.Substring (0, columnWidth - TRUNCATION_SYMBOL.Length) + TRUNCATION_SYMBOL;
+                return Value.Substring(0, columnWidth - TRUNCATION_SYMBOL.Length) + TRUNCATION_SYMBOL;
             }
-            return Value.PadRight (columnWidth);
+            return Value.PadRight(columnWidth);
         }
 
-        private void printColumnSeparator () {
+        private void printColumnSeparator() {
             // Write a column separator
-            foreach (DbColumn col in m_columns)
-            {
-                int columnWidth = getPrintedColumnWidth (col);
-                Console.Write("{0} ", pad ('-', columnWidth));
+            foreach (DbColumn col in m_columns) {
+                int columnWidth = getPrintedColumnWidth(col);
+                Console.Write("{0} ", pad('-', columnWidth));
             }
             Console.WriteLine();
         }
 
-        private void checkColumnWidth (int columnWidth) 
+        private void checkColumnWidth(int columnWidth) 
         {
-            if (m_widestColumnRead < columnWidth)
-                m_widestColumnRead = columnWidth;            
+            if (m_widestColumnRead < columnWidth) {
+                m_widestColumnRead = columnWidth;  
+            }          
         }
 
-        private int getPrintedColumnWidth (DbColumn column)
+        private int getPrintedColumnWidth(DbColumn column)
         { 
-            int columnMinWidth = Math.Max (column.ColumnName.Length, TRUNCATION_SYMBOL.Length);
-            if (m_maxColumnWidth < 0)
-                return Math.Max (columnMinWidth, (int)column.ColumnSize);
-            return Math.Max (columnMinWidth, m_maxColumnWidth);  
+            int columnMinWidth = Math.Max(column.ColumnName.Length, TRUNCATION_SYMBOL.Length);
+            if(m_maxColumnWidth < 0) {
+                return Math.Max(columnMinWidth, (int)column.ColumnSize);
+            }
+            return Math.Max(columnMinWidth, m_maxColumnWidth);  
         }
 
-        private string pad (char value, int length)
+        private string pad(char value, int length)
         {
-            string paddedString = "".PadRight (length);
-            if (value == ' ')
-            {
+            string paddedString = "".PadRight(length);
+            if (value == ' ') {
                 return paddedString;
             }
-            return paddedString.Replace (' ', value);
+            return paddedString.Replace(' ', value);
         }
     }
+    
--- ADO.NET/SampleApp.cs
+++ ADO.NET/SampleApp.cs
@@ -10,18 +10,17 @@ internal class SampleApp
         using (VerticaConnection connection = new VerticaConnection())
         {
             connection.ConnectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
-            Console.WriteLine ("Vertica ADO.net sample application.");
-            Console.WriteLine ("-----------------------------------");
-            Console.WriteLine ("Running on {0}.", System.Environment.MachineName);
-            Console.WriteLine ("Using connection string: {0}.", connection.ConnectionString);
+            Console.WriteLine("Vertica ADO.NET sample application.");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Running on {0}.", System.Environment.MachineName);
+            Console.WriteLine("Using connection string: {0}.", connection.ConnectionString);
 
             try {
                 connection.Open();
             } 
-            catch (Exception e)
-            {
+            catch (Exception e) {
                 Console.WriteLine("Could not connect to {0}", connection.ConnectionString);
-                Console.WriteLine (e);
+                Console.WriteLine(e);
                 return;
             }
 
@@ -30,12 +29,11 @@ internal class SampleApp
                 using (VerticaCommand command = connection.CreateCommand())
                 {
                     command.CommandText = ConfigurationManager.AppSettings["Query"];
-                    Console.WriteLine ("Query: {0}", command.CommandText);
+                    Console.WriteLine("Query: {0}", command.CommandText);
                     // Read the query results
-                    using (VerticaDataReader reader = command.ExecuteReader())
-                    {
-                        ResultSetPrinter printer = new ResultSetPrinter (reader);
-                        printer.printResults ();
+                    using (VerticaDataReader reader = command.ExecuteReader()) {
+                        ResultSetPrinter printer = new ResultSetPrinter(reader);
+                        printer.printResults();
                     }
                 }
             }             
