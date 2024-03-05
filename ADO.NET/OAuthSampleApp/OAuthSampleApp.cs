d0207bd4742784bc860b1bd754ce5239b2574683
Kevin Karch <kevinkarch88@gmail.com>
2024-03-05 11:02:39 -0500

    added token logic to ado sample app

--- ADO.NET/OAuthSampleApp/OAuthSampleApp.cs
+++ ADO.NET/OAuthSampleApp/OAuthSampleApp.cs
@@ -1,75 +1,216 @@
 ﻿using Vertica.Data.VerticaClient;
 using Newtonsoft.Json.Linq;
 using System.Configuration;
+using System.Runtime.InteropServices;
 
 internal class OAuthSampleApp
 {
-    static void Main(string[] args)
-    {
-        VerticaConnectionStringBuilder vcsb = new VerticaConnectionStringBuilder();
-        vcsb.ConnectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+    private static VerticaConnectionStringBuilder connectionStringBuilder;
+    private static VerticaConnection vConnection;
+    private const string OAUTH_ACCESS_TOKEN = "OAUTH_SAMPLE_ACCESS_TOKEN";
+    private const string OAUTH_REFRESH_TOKEN = "OAUTH_SAMPLE_REFRESH_TOKEN";
 
-        ConnectToDatabase(vcsb.ConnectionString);
+    static async Task Main(string[] args)
+    {
+        connectionStringBuilder = new VerticaConnectionStringBuilder();
+        connectionStringBuilder.ConnectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+        await SetUp();
+
+        // change the user from the dbadmin to OAuth user
+        connectionStringBuilder.User = ConfigurationManager.AppSettings["User"];
+
+        await EnsureAccessToken();
+        await ConnectToDatabase();
+        await TearDown();
     }
 
-    public static void ConnectToDatabase(string connectionString)
+    // admin must log in and setup the user for OAuth
+    // steps can also be followed in vsql
+    static async Task SetUp()
     {
-        try
-        {
-            using (VerticaConnection conn = new VerticaConnection(connectionString))
-            {
-                Console.WriteLine("Attempting to connect with connection string: " + ResultSetPrinter.printableConnectionString(connectionString));
-                conn.Open();
+        vConnection = new VerticaConnection(connectionStringBuilder.ConnectionString);
+        vConnection.Open();
 
-                using (VerticaCommand command = conn.CreateCommand())
+        string USER = ConfigurationManager.AppSettings["User"];
+        string CLIENT_ID = ConfigurationManager.AppSettings["ClientId"];
+        string CLIENT_SECRET = ConfigurationManager.AppSettings["ClientSecret"];
+        string INTROSPECT_URL = ConfigurationManager.AppSettings["TokenUrl"] + "introspect";
+
+        VerticaCommand cmd = new VerticaCommand("DROP USER IF EXISTS " + USER + " CASCADE", vConnection);
+        cmd.ExecuteNonQuery();
+        cmd.CommandText = "DROP AUTHENTICATION IF EXISTS adooauth CASCADE";
+        cmd.ExecuteNonQuery();
+        cmd.CommandText = "CREATE USER " + USER;
+        cmd.ExecuteNonQuery();
+        cmd.CommandText = "CREATE AUTHENTICATION adooauth METHOD 'oauth'" + GetAuthHost();
+        cmd.ExecuteNonQuery();
+        cmd.CommandText = "GRANT AUTHENTICATION adooauth TO " + USER;
+        cmd.ExecuteNonQuery();
+        cmd.CommandText = "GRANT ALL ON SCHEMA PUBLIC TO " + USER;
+        cmd.ExecuteNonQuery();
+        cmd.CommandText = "ALTER AUTHENTICATION adooauth SET client_id = '" + CLIENT_ID + "'";
+        cmd.ExecuteNonQuery();
+        cmd.CommandText = "ALTER AUTHENTICATION adooauth SET client_secret = '" + CLIENT_SECRET + "'";
+        cmd.ExecuteNonQuery();
+        cmd.CommandText = "ALTER AUTHENTICATION adooauth SET introspect_url = '" + INTROSPECT_URL + "'";
+        cmd.ExecuteNonQuery();
+    }
+
+    // removes the oauth user and closes the connection for the admin
+    public static async Task TearDown()
+    {
+        string USER = ConfigurationManager.AppSettings["User"];
+        VerticaCommand cmd = new VerticaCommand("DROP USER IF EXISTS " + USER + " CASCADE", vConnection);
+        cmd.ExecuteNonQuery();
+        cmd.CommandText = "DROP AUTHENTICATION IF EXISTS adooauth CASCADE";
+        cmd.ExecuteNonQuery();
+
+        vConnection.Close();
+    }
+
+    // if no access token is set, obtains and sets first access and refresh tokens
+    static async Task EnsureAccessToken()
+    {
+        string accessToken = Environment.GetEnvironmentVariable(OAUTH_ACCESS_TOKEN, EnvironmentVariableTarget.User);
+
+        if (string.IsNullOrEmpty(accessToken))
+        {
+            // Obtain first access token
+            Console.WriteLine("Access token not found. Obtaining new token...");
+            await GetAndSetTokens("password");
+        }
+
+        SetAccessToken();
+    }
+
+    // only the access token is set in the connection string
+    static void SetAccessToken()
+    {
+        string accessToken = Environment.GetEnvironmentVariable(OAUTH_ACCESS_TOKEN, EnvironmentVariableTarget.User);
+        connectionStringBuilder["OAuthAccessToken"] = accessToken;
+    }
+
+    // OAuth user attempts to connect with the access token
+    // If it fails, we try to get a new access token with the refresh token
+    // If the refresh token is expired, we get new access and refresh tokens
+    public static async Task ConnectToDatabase()
+    {
+        int retryCount = 0;
+
+        while (retryCount <= 1)
+        {
+            try
+            {
+                using (VerticaConnection conn = new VerticaConnection(connectionStringBuilder.ConnectionString))
                 {
-                    Console.WriteLine("Connection Successful");
-                    command.CommandText = ConfigurationManager.AppSettings["Query"];
-                    using (VerticaDataReader reader = command.ExecuteReader())
+                    Console.WriteLine("Attempting to connect with connection string: " + ResultSetPrinter.printableConnectionString(connectionStringBuilder.ConnectionString));
+                    conn.Open();
+
+                    using (VerticaCommand command = conn.CreateCommand())
                     {
-                        Console.WriteLine("Executing Query: " + command.CommandText);
-                        ResultSetPrinter printer = new ResultSetPrinter(reader);
-                        printer.printResults();
-                        Console.WriteLine("Query Executed. Exiting.");
+                        Console.WriteLine("Connection Successful");
+                        command.CommandText = ConfigurationManager.AppSettings["Query"];
+                        using (VerticaDataReader reader = command.ExecuteReader())
+                        {
+                            Console.WriteLine("Executing Query: " + command.CommandText);
+                            ResultSetPrinter printer = new ResultSetPrinter(reader);
+                            printer.printResults();
+                            Console.WriteLine("Query Executed. Exiting.");
+
+                            break;
+                        }
                     }
                 }
             }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.Message);
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                if (retryCount > 0) { break; }
+                try
+                {
+                    await GetAndSetTokens("refresh_token");
+                }
+                catch (HttpRequestException hre)
+                {
+                    Console.WriteLine(hre.Message);
+                    Console.WriteLine("Attempting to get new access and refresh tokens");
+                    await GetAndSetTokens("password");
+                }
+
+                SetAccessToken();
+                retryCount++;
+            }
         }
     }
-
-    // returns an access token that you can add to the connection string
-    public static async Task<string> GetToken(string tokenType, string clientID, string clientSecret, string user, string password, string tokenUrl)
+    public static async Task GetAndSetTokens(string grantType)
     {
+        // Read OAuth parameters from app.config
+        string clientId = ConfigurationManager.AppSettings["ClientId"];
+        string clientSecret = ConfigurationManager.AppSettings["ClientSecret"];
+        string user = ConfigurationManager.AppSettings["User"];
+        string password = ConfigurationManager.AppSettings["Password"];
+        string tokenUrl = ConfigurationManager.AppSettings["TokenUrl"];
+
         HttpClient httpClient = new HttpClient();
 
         try
         {
-            var content = new FormUrlEncodedContent(new Dictionary<string, string>
+            Dictionary<string, string> formData;
+
+            if (grantType == "password")
+            {
+                formData = new Dictionary<string, string>
             {
                 {"grant_type", "password"},
-                {"client_id", clientID},
+                {"client_id", clientId},
                 {"client_secret", clientSecret},
                 {"username", user},
                 {"password", password}
-            });
+            };
+            }
+            else if (grantType == "refresh_token")
+            {
+                string refreshToken = Environment.GetEnvironmentVariable(OAUTH_REFRESH_TOKEN, EnvironmentVariableTarget.User);
+
+                formData = new Dictionary<string, string>
+            {
+                {"grant_type", "refresh_token"},
+                {"client_id", clientId},
+                {"client_secret", clientSecret},
+                {"refresh_token", refreshToken}
+            };
+            }
+            else
+            {
+                throw new ArgumentException("Invalid grant type");
+            }
+
+            var content = new FormUrlEncodedContent(formData);
 
             var response = await httpClient.PostAsync(tokenUrl, content);
 
-            response.EnsureSuccessStatusCode(); // Ensure the request was successful
+            response.EnsureSuccessStatusCode();
 
             var result = await response.Content.ReadAsStringAsync();
             JObject jObject = JObject.Parse(result);
-            return jObject["access_token"].ToString();
+
+            // Set the access token as an environment variable
+            Environment.SetEnvironmentVariable(OAUTH_ACCESS_TOKEN, jObject["access_token"].ToString(), EnvironmentVariableTarget.User);
+
+            // Set the refresh token as an environment variable
+            Environment.SetEnvironmentVariable(OAUTH_REFRESH_TOKEN, jObject["refresh_token"].ToString(), EnvironmentVariableTarget.User);
         }
-        catch (HttpRequestException ex)
+        catch (Exception ex)
         {
             Console.WriteLine("Error getting access token: " + ex.Message);
             throw;
         }
+    }
+
+    static string GetAuthHost()
+    {
+        return RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ? "LOCAL" : "HOST '0.0.0.0/0'";
     }
 }
 
--- ADO.NET/OAuthSampleApp/README.md
+++ ADO.NET/OAuthSampleApp/README.md
@@ -2,18 +2,28 @@
 
 The Vertica ADO.NET driver now allows connecting through OAuth access tokens. 
 
-This sample application was built in .NET 6.0 can be run in .NET Framework 4.6+ or .NET Core 3.1+.
+This sample application was built in .NET 6.0 and can be run in .NET Framework 4.6+ or .NET Core 3.1+.
 
 # Prerequisites
 
 For information on installing .NET, starting a Vertica docker container, and how to reference a NuGet package, see the README.md in the ADO.NET base directory.
 
-# Adding OAuth to the server with VSQL
+# Using OAuth to login
 
-You must first configure the Vertica server for OAuth as described here: https://docs.vertica.com/24.1.x/en/security-and-authentication/client-authentication/oauth-2-0-authentication/configuring-oauth-authentication/
+You must first configure the Vertica server for OAuth as described here: https://docs.vertica.com/24.1.x/en/security-and-authentication/client-authentication/oauth-2-0-authentication/configuring-oauth-authentication/.
+The steps can also be done programatically as shown in the SetUp method.
 
-The doc describes how to get an access token through Curl, but the sample app has a function for getting access tokens if you prefer a programmatic approach.
+The doc describes how to get an access token through Curl, but the sample app has functions for getting access and refresh tokens.
+The authentication steps are:
+1. Use the OAuth credentials from the app.config to set an access and refresh token.
+2. Attempt to login with the access token. If it's expired, use the refresh token to get a new access token and login with that.
+3. If the refresh token is expired, repeat step 1.
+
+This app stores confidential information like the client secret in the app.config file for ease of use. Use proper secret management tools in production.
+
+Note: The token url should be in the form of "http://192.168.0.255:8080/realms/test/protocol/openid-connect/token/"
 
 # Running the app
 
-To run the sample app, you need to fill out the app.config file, which contains the connection string and query. The file has the default host, port, database, and user with an empty password and access token. To connect using OAuth, add your access token to the connection string, leaving the password blank.
+To run the sample app, fill out the app.config file. The connection string is for the default superuser.
+To connect using OAuth, add your client ID, client secret, username, password, and token url that you configured with your IDP.
