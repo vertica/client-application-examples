c7942001cc715e196ca1459a03f5a2c80032c389
rvpanchal1202 <149764336+rvpanchal1202@users.noreply.github.com>
2024-04-17 09:37:36 +0530

    Update OAuthSampleApp.java, Added refresh token api to get new access token
    using the refresh token, we can get the new access token in case of a wrong access token or an expired access token. 
    
    Added API to get the new access_token using refresh_token
    
    note: When we request for new access_token using refresh token, This request will give us both the tokens. which we can save later to original tokens and start using new Tokens instead of old tokens

--- JDBC/OAuthSampleApp/src/main/java/OAuthSampleApp.java
+++ JDBC/OAuthSampleApp/src/main/java/OAuthSampleApp.java
@@ -21,6 +21,23 @@ import java.sql.SQLInvalidAuthorizationSpecException;
 import java.util.Properties;
 import java.util.concurrent.Callable;
 
+import java.io.OutputStream;
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.DataOutputStream;
+import java.net.URL;
+import javax.net.ssl.*;
+import java.net.HttpURLConnection;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.security.KeyStore;
+
+import com.google.gson.JsonParser;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
@@ -46,7 +63,83 @@ public class OAuthSampleApp implements Callable<Integer> {
 
     @Option(names = { "-s", "--client-secret" }, description = "Client Secret")
     private String clientSecret = "";
+	
+	private static final String OAUTH_CLIENT_ID = System.getenv("OAUTH_CLIENT_ID");
+    private static final String OAUTH_REALM = System.getenv("OAUTH_REALM");
+    private static final String OAUTH_TOKEN_USER = System.getenv("OAUTH_TOKEN_USER");
+    private static final String OAUTH_TOKEN_PASSWORD = System.getenv("OAUTH_TOKEN_PASSWORD");
 
+    private static String OAUTH_SERVER = System.getenv("OAUTH_SERVER"); 
+    private static String OAUTH_HTTP_PORT =  System.getenv("OAUTH_HTTP_PORT");
+
+    private static String getTokenUrl()
+    {
+        return "http://" + OAUTH_SERVER + ":" + OAUTH_HTTP_PORT + "/realms/" + OAUTH_REALM + "/protocol/openid-connect/token";
+    }
+
+    private static String getIssuerUri()
+    {
+        return "http://" + OAUTH_SERVER + ":" + OAUTH_HTTP_PORT + "/realms/" + OAUTH_REALM;
+    }
+
+    private static String getHttpsTokenUrl()
+    {
+        return "http://" + OAUTH_SERVER + ":" + OAUTH_HTTP_PORT + "/realms/" + OAUTH_REALM + "/protocol/openid-connect/token";
+    }
+
+    private static String getDiscoveryUrl()
+    {
+        return "http://" + OAUTH_SERVER + ":" + OAUTH_HTTP_PORT + "/realms/" + OAUTH_REALM + "/.well-known/openid-configuration";
+    }
+
+    private void getTokenRefresh(String username, String password, String clientId, String clientSecret, String tokenUrl) throws Exception
+    {
+		String postOpts = "grant_type=refresh_token&client_secret=" + clientSecret + "&client_id=" + clientId + "&refresh_token=" + refreshToken;
+         byte[] postData = postOpts.getBytes("UTF-8");
+        int postDataLength = postData.length;
+        URL url = new URL(tokenUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        try {
+            connection.setDoOutput(true);
+            connection.setUseCaches(false);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.setRequestProperty("Content-Length", Integer.toString(postDataLength));
+            connection.setRequestProperty("Accept", "application/json");
+			connection.getOutputStream().write(postData);
+           	BufferedInputStream in = new BufferedInputStream(connection.getInputStream());
+    	    ByteArrayOutputStream buf = new ByteArrayOutputStream();
+            for (int result = in.read(); result != -1; result = in.read()) {
+               	buf.write((byte) result);
+	        }
+    	    String res = buf.toString("UTF-8");
+        	JsonElement jElement = new JsonParser().parse(res);
+            JsonObject jObject = jElement.getAsJsonObject();
+			accessToken = jObject.get("access_token").getAsString();
+			refreshToken = jObject.get("refresh_token").getAsString();
+			
+			System.out.println("\n\nNew Access Token : " + accessToken + "\n\nNew Refresh Token : " + refreshToken ); 
+
+        } catch (IOException e) {
+            String res = "";
+            try {
+                BufferedInputStream in = new BufferedInputStream(connection.getErrorStream());
+                ByteArrayOutputStream buf = new ByteArrayOutputStream();
+                for (int result = in.read(); result != -1; result = in.read()) {
+                    buf.write((byte) result);
+                }
+                res = buf.toString("UTF-8");
+            } catch (IOException inputStreamReadException) {
+                //Improper setup. Passes in SF, fails in Devjail. Skip when this happens, but print the error.
+                inputStreamReadException.printStackTrace();
+                System.out.println("Error when fetching token endpoints. Skipping test.");
+            }
+			throw e;
+        } finally {
+            connection.disconnect();
+        }
+    }
+	
     private static Connection connectToDB(String host, String port, String dbName, String accessToken,
             String refreshToken, String clientSecret) throws SQLException {
         Properties jdbcOptions = new Properties();
@@ -77,6 +170,11 @@ public class OAuthSampleApp implements Callable<Integer> {
             Connection conn = connectToDB(host, port, dbName, accessToken, refreshToken, clientSecret);
             ResultSet rs = executeQuery(conn);
             printResults(rs);
+			try{
+				getTokenRefresh(OAUTH_TOKEN_USER, OAUTH_TOKEN_PASSWORD, OAUTH_CLIENT_ID, clientSecret, getTokenUrl());
+			}catch(Exception e){
+				throw e;
+			}
             conn.close();
         } catch (SQLTransientConnectionException connException) {
             System.out.print("Network connection issue: ");
