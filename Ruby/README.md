84e7f85ea2da76cb583b0c7378944bca8488ade7
Siting Ren <sren2@opentext.com>
2024-04-24 11:17:03 -0400

    add ruby

---
+++ Ruby/README.md
@@ -0,0 +1,11 @@
+# Pre-requisites
+
+- JRuby (tested with version 9.3.8.0 on Linux)
+- Vertica JDBC
+
+# Running the Ruby sample app
+
+Run the sample app using the following command:
+```
+jruby RubySampleApp.rb
+```
---
+++ Ruby/RubySampleApp.rb
@@ -0,0 +1,46 @@
+#!/usr/bin/env jruby
+
+require 'java'
+
+# There are a couple of ways to tell JRuby where to find classes.  The first
+# is with the JVM CLASSPATH environmental variable:
+#
+#   CLASSPATH=/path/to/vertica/jdbc.jar:$CLASSPATH ./RubySampleApp.rb
+#
+# The other, which we're going to use here, is to require the jar file in
+# the program itself:
+require '/path/to/vertica-jdbc-x.x.x-x.jar'
+
+java_import 'java.lang.System'
+java_import 'java.sql.DriverManager'
+java_import 'java.sql.SQLException'
+java_import 'java.sql.SQLInvalidAuthorizationSpecException'
+java_import 'java.util.Properties'
+
+
+begin
+    myProp = Properties.new()
+    myProp.put("user", "dbadmin")
+    myProp.put("loginTimeout", "35")
+
+    conn = DriverManager.getConnection("jdbc:vertica://localhost:5433/dbname", myProp)
+    System.out.println('Connected!')
+
+    # Execute queries through JDBC
+    stmt = conn.createStatement()
+    sql = "SELECT node_name, start_time, end_time, average_cpu_usage_percent FROM v_monitor.cpu_usage LIMIT 5"
+    System.out.println("Query: " + sql)
+    rs = stmt.executeQuery(sql)
+    System.out.println("Query result: ")
+    while rs.next()
+        System.out.println(rs.getString(1) + " | " + rs.getString(2) + " | " + 
+			   rs.getString(3) + " | " + rs.getString(4))
+    end
+    conn.close()
+rescue SQLInvalidAuthorizationSpecException => e
+    System.out.print("Could not log into database: ")
+    System.out.println(e)
+    System.out.println("Check the login credentials and try again.")
+rescue SQLException => e
+    System.out.println(e)
+end
