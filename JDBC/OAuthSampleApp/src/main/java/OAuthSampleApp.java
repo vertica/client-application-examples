736b5e96ecb34f48f808d64153b962f620603f73
rvpanchal1202 <149764336+rvpanchal1202@users.noreply.github.com>
2024-05-28 13:08:22 +0530

    Update OAuthSampleApp.java
    Review comments addressed, 
    -> Code alliagnment 
    -> Contant and non constant variables consistency adjusted,
    -> functions are rearranged as per ADO.Net Sample app

--- JDBC/OAuthSampleApp/src/main/java/OAuthSampleApp.java
+++ JDBC/OAuthSampleApp/src/main/java/OAuthSampleApp.java
@@ -1,3 +1,4 @@
+
 // (c) Copyright [2022-2023] Open Text.
 // Licensed under the Apache License, Version 2.0 (the "License");
 // You may not use this file except in compliance with the License.
@@ -36,59 +37,64 @@ import com.google.gson.JsonObject;
 import com.vertica.jdbc.VerticaConnection;
 
 public class OAuthSampleApp {
+	private static final String OAUTH_ACCESS_TOKEN_VAR_STRING = "OAUTH_SAMPLE_ACCESS_TOKEN";
+	private static final String OAUTH_REFRESH_TOKEN_VAR_STRING = "OAUTH_SAMPLE_REFRESH_TOKEN";
+
 	private static Properties prop;
 	private static Map<String, String> csProp = new HashMap<String, String>();
-	private static String OAUTH_ACCESS_TOKEN_VAR_STRING = "OAUTH_SAMPLE_ACCESS_TOKEN";
-	private static String OAUTH_REFRESH_TOKEN_VAR_STRING = "OAUTH_SAMPLE_REFRESH_TOKEN";
 	private static Connection vConnection;
 
-	// Get jdbc connection string from provided configuration
-	private static void getcsProp(String args) {
-		String[] entries = args.split(";");
-		for (String entry : entries) {
-			String[] pair = entry.split("=");
-			csProp.put(pair[0], pair[1]);
+	// main function, Call starts from here
+	public static void main(String[] args) {
+		try {
+			loadProperties();
+			setUpDbForOAuth();
+			ensureAccessToken();
+			connectToDatabase();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception unreportedEx) {
+			unreportedEx.printStackTrace();
+		} finally {
+			tearDown();
 		}
+		System.exit(0);
 	}
 
-	private static String getConnectionString() {
-		return "jdbc:vertica://" + csProp.get("Host") + ":" + csProp.get("Port") + "/" + csProp.get("Database")
-				+ "?user=" + csProp.get("User") + "&password=" + csProp.get("Password");
-	}
-
-	// Get the parameters from the connection String
-	private static String getParam(String paramName) {
-		return csProp.get(paramName);
-	}
-
-	// Add/Create Authentication record in database. Create User and grant the
-	// permissions for User
-	private static void SetUpDbForOAuth() throws Exception {
+	// admin must log in and setup the user for OAuth
+	// steps can also be followed in vsql
+	private static void setUpDbForOAuth() throws Exception{
 		String connectionString = getConnectionString();
+		String user = prop.getProperty("User");
+		String clientId = prop.getProperty("ClientId");
+		String clientSecret = prop.getProperty("ClientSecret");
+		String discoveryUrl = prop.getProperty("DiscoveryUrl");
+		String queries[]= {
+				"DROP USER IF EXISTS " + user + " CASCADE;",
+				"DROP AUTHENTICATION IF EXISTS v_oauth CASCADE;",
+				"CREATE AUTHENTICATION v_oauth METHOD 'oauth' LOCAL;",
+				"ALTER AUTHENTICATION v_oauth SET client_id = '" + clientId + "';",
+				"ALTER AUTHENTICATION v_oauth SET client_secret = '" + clientSecret + "';",
+				"ALTER AUTHENTICATION v_oauth SET discovery_url = '" + discoveryUrl + "';",
+				"CREATE USER " + user + ";",
+				"GRANT AUTHENTICATION v_oauth TO " + user + ";",
+				"GRANT ALL ON SCHEMA PUBLIC TO " + user + ";"
+				};
 		vConnection = DriverManager.getConnection(connectionString);
-		String USER = prop.getProperty("User");
-		String CLIENT_ID = prop.getProperty("ClientId");
-		String CLIENT_SECRET = prop.getProperty("ClientSecret");
-		String DISCOVERY_URL = prop.getProperty("DiscoveryUrl");
 		Statement st = vConnection.createStatement();
-		st.execute("DROP USER IF EXISTS " + USER + " CASCADE;");
-		st.execute("DROP AUTHENTICATION IF EXISTS v_oauth CASCADE;");
-		st.execute("CREATE AUTHENTICATION v_oauth METHOD 'oauth' LOCAL;");
-		st.execute("ALTER AUTHENTICATION v_oauth SET client_id= '" + CLIENT_ID + "';");
-		st.execute("ALTER AUTHENTICATION v_oauth SET client_secret= '" + CLIENT_SECRET + "';");
-		st.execute("ALTER AUTHENTICATION v_oauth SET discovery_url = '" + DISCOVERY_URL + "';");
-		st.execute("CREATE USER " + USER + ";");
-		st.execute("GRANT AUTHENTICATION v_oauth TO " + USER + ";");
-		st.execute("GRANT ALL ON SCHEMA PUBLIC TO " + USER + ";");
+		
+		for (String query: queries) {
+			st.execute(query);
+		}
 		st.close();
 	}
 
-	// Dispose the authentication record from database
-	private static void TearDown() {
+	// removes the oauth user and closes the admin connection
+	private static void tearDown() {
 		try {
 			Statement st = vConnection.createStatement();
-			String USER = prop.getProperty("User");
-			st.executeUpdate("DROP USER IF EXISTS " + USER + " CASCADE");
+			String user = prop.getProperty("User");
+			st.executeUpdate("DROP USER IF EXISTS " + user + " CASCADE");
 			st.executeUpdate("DROP AUTHENTICATION IF EXISTS v_oauth CASCADE");
 			vConnection.close();
 		} catch (Exception e) {
@@ -96,17 +102,35 @@ public class OAuthSampleApp {
 		}
 	}
 
-	// Connect to Database using access Token
-	private static Connection getConnection(String accessToken) throws SQLException {
-		Properties jdbcOptions = new Properties();
-		jdbcOptions.put("oauthaccesstoken", accessToken);
-		return DriverManager.getConnection(
-				"jdbc:vertica://" + getParam("Host") + ":" + getParam("Port") + "/" + getParam("Database"),
-				jdbcOptions);
+	// If access token is Invalid/Expired, Get new tokens using password/refresh grant
+	private static void ensureAccessToken() throws Exception {
+		String accessToken = System.getenv(OAUTH_ACCESS_TOKEN_VAR_STRING);
+		String refreshToken = System.getenv(OAUTH_REFRESH_TOKEN_VAR_STRING);
+		try {
+			if (null == accessToken || accessToken.isEmpty()) {
+				if (null == refreshToken || refreshToken.isEmpty()) {
+					// Obtain first access token and refresh Tokens
+					System.out.println("Access token is invalid/expired, trying to do refresh");
+					getTokensByPasswordGrant();
+				} else {
+					try {
+						System.out.println("Attempting to use given refresh token.");
+						getTokensByRefreshGrant();
+					} catch (Exception e) {
+						System.out.println("Initial refresh token has expired. Getting new access and refresh tokens.");
+						getTokensByPasswordGrant();
+					}
+				}
+			} else {
+				System.setProperty(OAUTH_ACCESS_TOKEN_VAR_STRING, accessToken);
+			}
+		} catch (Exception e) {
+			throw e;
+		}
 	}
 
 	// Test connection using access token and test database query result
-	private static void ConnectToDatabase() throws SQLException {
+	private static void connectToDatabase() throws SQLException {
 		int connAttemptCount = 0;
 		while (connAttemptCount <= 1) {
 			try {
@@ -126,11 +150,11 @@ public class OAuthSampleApp {
 					break;
 				}
 				try {
-					GetTokensByRefreshGrant();
+					getTokensByRefreshGrant();
 				} catch (Exception e1) {
 					try {
 						System.out.println("Refresh token is invalid/Expired, Getting new tokens");
-						GetTokensByPasswordGrant();
+						getTokensByPasswordGrant();
 					} catch (Exception e2) {
 						e2.printStackTrace();
 					}
@@ -138,6 +162,112 @@ public class OAuthSampleApp {
 				++connAttemptCount;
 			}
 		}
+	}
+
+	// password grant logs into the IDP using credentials in app.config
+	public static void getTokensByPasswordGrant() throws Exception {
+		Map<String, String> formData = new HashMap<String, String>();
+		formData.put("grant_type", "password");
+		formData.put("client_id", prop.getProperty("ClientId"));
+		formData.put("client_secret", prop.getProperty("ClientSecret"));
+		formData.put("username", prop.getProperty("User"));
+		formData.put("password", prop.getProperty("Password"));
+		getAndSetTokens(formData);
+	}
+
+	// refresh grant uses the refresh token to get the new access and refresh token
+	public static void getTokensByRefreshGrant() throws Exception {
+		Map<String, String> formData = new HashMap<String, String>();
+		formData.put("grant_type", "refresh_token");
+		formData.put("client_id", prop.getProperty("ClientId"));
+		formData.put("client_secret", prop.getProperty("ClientSecret"));
+		formData.put("refresh_token", System.getProperty(OAUTH_REFRESH_TOKEN_VAR_STRING));
+		getAndSetTokens(formData);
+	}
+
+	// get and set tokens from IDP
+	private static void getAndSetTokens(Map<String, String> formData) throws Exception {
+		try {
+			String postOpts = getEncodedParamsString(formData);
+			byte[] postData = postOpts.getBytes("UTF-8");
+			int postDataLength = postData.length;
+			URL url = new URL(prop.getProperty("TokenUrl"));
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			try {
+				connection.setDoOutput(true);
+				connection.setUseCaches(false);
+				connection.setRequestMethod("POST");
+				connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+				connection.setRequestProperty("Content-Length", Integer.toString(postDataLength));
+				connection.setRequestProperty("Accept", "application/json");
+				connection.getOutputStream().write(postData);
+				BufferedInputStream in = new BufferedInputStream(connection.getInputStream());
+				ByteArrayOutputStream buf = new ByteArrayOutputStream();
+				readResult(in, buf);
+				String res = buf.toString("UTF-8");
+				JsonElement jElement = JsonParser.parseString(res);
+				JsonObject jObject = jElement.getAsJsonObject();
+				// set Tokens as System Properties - new values to access_token and
+				// refresh_token
+				String accessToken = jObject.has("access_token") ? jObject.get("access_token").getAsString() : null;
+				String refreshToken = jObject.has("refresh_token") ? jObject.get("refresh_token").getAsString() : null;
+				if (null == accessToken) {
+					throw new Exception(
+							"Access/refresh token is null, Please verify the config.properties for proper inputs.");
+				}
+				System.setProperty(OAUTH_ACCESS_TOKEN_VAR_STRING, accessToken);
+				if (null != refreshToken && !refreshToken.isEmpty()) {
+					System.setProperty(OAUTH_REFRESH_TOKEN_VAR_STRING, refreshToken);
+				}
+			} catch (UnsupportedEncodingException uee) {
+				uee.printStackTrace();
+			} catch (Exception e) {
+				String res = "";
+				try {
+					BufferedInputStream in = new BufferedInputStream(connection.getErrorStream());
+					ByteArrayOutputStream buf = new ByteArrayOutputStream();
+					readResult(in, buf);
+					res = buf.toString("UTF-8");
+				} catch (Exception ex) {
+					// Skip when this happens, but print the error.
+					ex.printStackTrace();
+				}
+				throw e;
+			} finally {
+				connection.disconnect();
+			}
+		} catch (IOException unreportedex) {
+			unreportedex.printStackTrace();
+		}
+	}
+
+	// Get jdbc connection string from provided configuration
+	private static void getcsProp(String args) {
+		String[] entries = args.split(";");
+		for (String entry : entries) {
+			String[] pair = entry.split("=");
+			csProp.put(pair[0], pair[1]);
+		}
+	}
+	
+	// get connection string for admin connection to setup OAuth Record
+	private static String getConnectionString() {
+		return "jdbc:vertica://" + csProp.get("Host") + ":" + csProp.get("Port") + "/" + csProp.get("Database")
+				+ "?user=" + csProp.get("User") + "&password=" + csProp.get("Password");
+	}
+
+	// Get the parameters from the connection String
+	private static String getParam(String paramName) {
+		return csProp.get(paramName);
+	}
+
+	// Connect to Database using access Token
+	private static Connection getConnection(String accessToken) throws SQLException {
+		Properties jdbcOptions = new Properties();
+		jdbcOptions.put("oauthaccesstoken", accessToken);
+		return DriverManager.getConnection(
+				"jdbc:vertica://" + getParam("Host") + ":" + getParam("Port") + "/" + getParam("Database"),
+				jdbcOptions);
 	}
 
 	// execute Simple query on database connection
@@ -169,27 +299,6 @@ public class OAuthSampleApp {
 		return result.toString();
 	}
 
-	// password grant logs into the IDP using credentials in app.config
-	public static void GetTokensByPasswordGrant() throws Exception {
-		Map<String, String> formData = new HashMap<String, String>();
-		formData.put("grant_type", "password");
-		formData.put("client_id", prop.getProperty("ClientId"));
-		formData.put("client_secret", prop.getProperty("ClientSecret"));
-		formData.put("username", prop.getProperty("User"));
-		formData.put("password", prop.getProperty("Password"));
-		GetAndSetTokens(formData);
-	}
-
-	// refresh grant uses the refresh token to get the new access and refresh token
-	public static void GetTokensByRefreshGrant() throws Exception {
-		Map<String, String> formData = new HashMap<String, String>();
-		formData.put("grant_type", "refresh_token");
-		formData.put("client_id", prop.getProperty("ClientId"));
-		formData.put("client_secret", prop.getProperty("ClientSecret"));
-		formData.put("refresh_token", System.getProperty(OAUTH_REFRESH_TOKEN_VAR_STRING));
-		GetAndSetTokens(formData);
-	}
-
 	// read result from Buffered input stream
 	private static ByteArrayOutputStream readResult(BufferedInputStream in, ByteArrayOutputStream buf) {
 		try {
@@ -200,89 +309,6 @@ public class OAuthSampleApp {
 			e.printStackTrace();
 		}
 		return buf;
-	}
-
-	// get and set tokens from IDP
-	private static void GetAndSetTokens(Map<String, String> formData) throws Exception {
-		try {
-			String postOpts = getEncodedParamsString(formData);
-			byte[] postData = postOpts.getBytes("UTF-8");
-			int postDataLength = postData.length;
-			URL url = new URL(prop.getProperty("TokenUrl"));
-			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-			try {
-				connection.setDoOutput(true);
-				connection.setUseCaches(false);
-				connection.setRequestMethod("POST");
-				connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-				connection.setRequestProperty("Content-Length", Integer.toString(postDataLength));
-				connection.setRequestProperty("Accept", "application/json");
-				connection.getOutputStream().write(postData);
-				BufferedInputStream in = new BufferedInputStream(connection.getInputStream());
-				ByteArrayOutputStream buf = new ByteArrayOutputStream();
-				readResult(in, buf);
-				String res = buf.toString("UTF-8");
-				JsonElement jElement = JsonParser.parseString(res);
-				JsonObject jObject = jElement.getAsJsonObject();
-				// set Tokens as System Properties - new values to access_token and
-				// refresh_token
-				String accessToken = jObject.has("access_token") ? jObject.get("access_token").getAsString() : null;
-				String refreshToken = jObject.has("refresh_token") ? jObject.get("refresh_token").getAsString() : null;
-				if (null == accessToken) {
-					throw new Exception(
-							"Access/refresh token is null, Please verify the config.properties for proper inputs.");
-				}
-				System.setProperty(OAUTH_ACCESS_TOKEN_VAR_STRING, accessToken);
-				if(null != refreshToken && !refreshToken.isEmpty()) {
-					System.setProperty(OAUTH_REFRESH_TOKEN_VAR_STRING, refreshToken);
-				}
-			} catch (UnsupportedEncodingException uee) {
-				uee.printStackTrace();
-			} catch (Exception e) {
-				String res = "";
-				try {
-					BufferedInputStream in = new BufferedInputStream(connection.getErrorStream());
-					ByteArrayOutputStream buf = new ByteArrayOutputStream();
-					readResult(in, buf);
-					res = buf.toString("UTF-8");
-				} catch (Exception ex) {
-					// Skip when this happens, but print the error.
-					ex.printStackTrace();
-				}
-				throw e;
-			} finally {
-				connection.disconnect();
-			}
-		} catch (IOException unreportedex) {
-			unreportedex.printStackTrace();
-		}
-	}
-
-	// If access token is Invalid/Expired, Get new tokens using password/refresh grant
-	private static void EnsureAccessToken() throws Exception {
-		try {
-			String accessToken = System.getenv(OAUTH_ACCESS_TOKEN_VAR_STRING);
-			String refreshToken = System.getenv(OAUTH_REFRESH_TOKEN_VAR_STRING);
-			if (null == accessToken || accessToken.isEmpty()) {
-				if (null == refreshToken || refreshToken.isEmpty()) {
-					// Obtain first access token and refresh Tokens
-					System.out.println("Access token is invalid/expired, trying to do refresh");
-					GetTokensByPasswordGrant();
-				} else {
-					try{
-						System.out.println("Attempting to use given refresh token.");
-						GetTokensByRefreshGrant();
-					}catch(Exception e){
-						System.out.println("Initial refresh token has expired. Getting new access and refresh tokens.");
-                    	GetTokensByPasswordGrant();
-					}
-				}
-			}else{
-				System.setProperty(OAUTH_ACCESS_TOKEN_VAR_STRING, accessToken);
-			}
-		} catch (Exception e) {
-			throw e;
-		}
 	}
 
 	// load the configuration properties
@@ -297,22 +323,4 @@ public class OAuthSampleApp {
 			ex.printStackTrace();
 		}
 	}
-
-	// main function, Call starts from here
-	public static void main(String[] args) {
-		try {
-			loadProperties();
-			SetUpDbForOAuth();
-			EnsureAccessToken();
-			ConnectToDatabase();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (Exception unreportedEx) {
-			unreportedEx.printStackTrace();
-		} finally {
-			TearDown();
-		}
-		System.exit(0);
-	}
 }
-
